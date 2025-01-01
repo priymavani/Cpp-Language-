5d0a2cefffe1efa3962fee95e8fcb66246e5e5f3
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-01-01 22:50:13 +0530

    Done 26 with2 approch

--- 21.cpp
+++ 21.cpp
@@ -1,16 +1,61 @@
 // 21. Write a program to convert a given string to uppercase.Example: Input: "hello", Output: "HELLO".
 
-//approch 1
+// //approch 1
+// #include<iostream>
+// using namespace std;
+// #include<string>
+// void uppercase(string str){
+//     int size = str.size();
+//     for(int i=0;i<size;i++){
+//         if(str[i] >= 'a' && str[i]<= 'z'){
+//         str[i] = str[i] - 32;
+//         }
+//     }
+//     cout << str;
+// }
+// int main(){
+//     uppercase("string");
+// }
+
+
+
+// // approch 2
+// #include<iostream>
+// using namespace std;
+// #include<string>
+// #include<cctype>
+
+// string toUpperCase(string str){
+//     for(int i =0 ; i< str.length(); i++){
+//         str[i] = toupper(str[i]);
+//     }
+//     return str;
+// }
+
+// int main(){
+
+//     string str ="priy";
+
+//     cout << toUpperCase(str) <<endl  ;
+//     return 0 ;
+
+// }
+
+// approch 3
 #include<iostream>
 using namespace std;
 #include<string>
-void uppercase(string str){
-    int size = str.size();
-    for(int i=0;i<size;i++){
-        str[i] = str[i] - 32;
-    }
-    cout << str;
+#include<algorithm>
+#include<cctype>
+
+string toUpperCase(string str){
+    transform(str.begin(),str.end() , str.begin() , ::toupper);
+    return str;
 }
+
 int main(){
-    uppercase("string");
+    string str = "priy mavani";
+
+    cout <<  toUpperCase(str) << endl ;
+    return 0 ;
 }
--- 26.cpp
+++ 26.cpp
@@ -1,2 +1,53 @@
 // 26. Write a program to check if a string ends with a specific character.Example: Input: ("codinggita", "a"), Output: true. 
 
+
+// // approch 1 
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// bool checkLastLetter(string str,char a ){
+//     int length = str.length();
+    
+//         if(length == 0 ){
+//             cout << "false" << endl ;
+//             return false ;
+//         }
+
+//         if(str[length - 1] == a){
+//             cout << "true" << endl ;
+//             return true ;
+//         }
+    
+//     cout << "false" << endl ;
+//     return false ;
+// }
+
+
+// int main(){
+//     string str = "codinggita";
+//     checkLastLetter(str , 'a');
+// }
+
+
+// approch 2  
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool chechLastLetter(string str, char ch){
+    return !str.empty() && str.back() == ch ;
+}
+
+int main(){
+
+    string str = "codinggita" ;
+    char ch  = 'a' ;
+
+     if (chechLastLetter(str , ch )) {
+        cout << "Output: true" << endl; 
+    } else {
+        cout << "Output: false" << endl;  
+    }
+
+}
